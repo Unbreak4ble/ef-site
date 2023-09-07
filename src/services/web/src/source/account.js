204d48291fbdd85dc14f1e1b00165651d6b4837a
Unbreak4ble <diego.gamere1921@outlook.com>
2023-09-06 22:55:53 -0400

    fixed modal bugs

--- src/services/web/src/source/account.js
+++ src/services/web/src/source/account.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, memo, createContext, useContext} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,6 +7,9 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { loadSessions, handleEvent, websocket, get_time, calcDate, loadCookies, getJobInfo } from "./utils.js";
+
+
+const modalContext = createContext("none");
 
 function Session({username, expiration, status}){
 	return (
@@ -92,24 +95,18 @@ class Sessions extends React.Component {
 	}
 }
 
-function SessionModal({prop_display}) {
-	const [display, setDisplay] = useState(prop_display);
-	
-	useEffect(() => {
-			console.log("updated", display);
-			if(display == "none")
-				setDisplay("flex");
-	}, [display]);
+function SessionModal(){
+	const [context, setContext] = useContext(modalContext);
 	
 	return (
-		<div className="modal" style={{display: display}}>
+		<div className="modal" style={{display: context.modal}}>
 			<div className="modal-body">
 				<div className="modal-bar">
 					<a>Add Session</a>
-					<div className="modal-close" onClick={() => setDisplay("none")}>
+					<div className="modal-close" onClick={() => setContext({ modal: "none" })}>
 						<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 26 26">
-<path d="M 21.734375 19.640625 L 19.636719 21.734375 C 19.253906 22.121094 18.628906 22.121094 18.242188 21.734375 L 13 16.496094 L 7.761719 21.734375 C 7.375 22.121094 6.746094 22.121094 6.363281 21.734375 L 4.265625 19.640625 C 3.878906 19.253906 3.878906 18.628906 4.265625 18.242188 L 9.503906 13 L 4.265625 7.761719 C 3.882813 7.371094 3.882813 6.742188 4.265625 6.363281 L 6.363281 4.265625 C 6.746094 3.878906 7.375 3.878906 7.761719 4.265625 L 13 9.507813 L 18.242188 4.265625 C 18.628906 3.878906 19.257813 3.878906 19.636719 4.265625 L 21.734375 6.359375 C 22.121094 6.746094 22.121094 7.375 21.738281 7.761719 L 16.496094 13 L 21.734375 18.242188 C 22.121094 18.628906 22.121094 19.253906 21.734375 19.640625 Z"></path>
-</svg>
+							<path d="M 21.734375 19.640625 L 19.636719 21.734375 C 19.253906 22.121094 18.628906 22.121094 18.242188 21.734375 L 13 16.496094 L 7.761719 21.734375 C 7.375 22.121094 6.746094 22.121094 6.363281 21.734375 L 4.265625 19.640625 C 3.878906 19.253906 3.878906 18.628906 4.265625 18.242188 L 9.503906 13 L 4.265625 7.761719 C 3.882813 7.371094 3.882813 6.742188 4.265625 6.363281 L 6.363281 4.265625 C 6.746094 3.878906 7.375 3.878906 7.761719 4.265625 L 13 9.507813 L 18.242188 4.265625 C 18.628906 3.878906 19.257813 3.878906 19.636719 4.265625 L 21.734375 6.359375 C 22.121094 6.746094 22.121094 7.375 21.738281 7.761719 L 16.496094 13 L 21.734375 18.242188 C 22.121094 18.628906 22.121094 19.253906 21.734375 19.640625 Z"></path>
+	</svg>
 					</div>
 				</div>
 				<div className="modal-content">
@@ -126,21 +123,24 @@ function SessionModal({prop_display}) {
 
 class Account extends React.Component {
 				state = {
-					modal: <></>
+					modal: "none"
 				}
-				
         constructor(props) {
           super(props);
         }
 
+				setstate(value){
+					return this.setState({...this.state, value});
+				}
+
         render() {
                 return (
 									<>
-										{
-											this.state.modal
-										}
+										<modalContext.Provider value={[this.state, this.setState.bind(this)]}>
+											<SessionModal/>
+										</modalContext.Provider>
 										<div className="flex">
-											<div className="append_session" onClick={() => this.setState({modal: <SessionModal prop_display="flex"/>}) }>
+											<div className="append_session" onClick={() => this.setState({ modal: "flex"}) }>
 												<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
 											</div>
 			  	    				<Sessions/>
